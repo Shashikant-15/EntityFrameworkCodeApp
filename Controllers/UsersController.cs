22583129ceba2caadf8e4b8ccc4eeb63213fea59
Shashikant-15 <shashikant.tanti@nashtechglobal.com>
2023-10-26 14:05:36 +0530

    Add project files.

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,71 @@
+﻿using EntityFrameworkCodeApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EntityFrameworkCodeApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserContext userContext;   // constructor
+
+        public  UsersController(UserContext userContext)
+        {
+            this.userContext = userContext;
+        }
+
+        [HttpGet]
+        [Route("GetUsers")] 
+        public List<Users>  GetUsers()
+        {
+            return  userContext.Users.ToList();
+        }
+
+        [HttpGet]
+        [Route("GetaUser")]
+        public Users GetaUser(int id)
+        {
+            return userContext.Users.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        [HttpPost]
+        [Route("AddUser")]
+        public string AddUser( Users users) 
+        {
+            string response = string.Empty;
+            userContext.Users.Add(users);
+            userContext.SaveChanges();
+            return "New User Added !!!";
+        }
+        [HttpPut]
+        [Route("UpdateUser")]
+        public string UpdateUser( Users users)
+        {
+            userContext.Entry(users).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            userContext.SaveChanges();
+
+            return "User successfully updated";
+        }
+        [HttpDelete]
+        [Route("DeleteUser")]
+        public string DeleteUser(int id)
+        {
+            Users users = userContext.Users.Where(x => x.Id == id).FirstOrDefault();
+
+            if (users != null)
+            {
+                userContext.Users.Remove(users);
+                userContext.SaveChanges();
+                return "User Deleted !!";
+            }
+            else
+            {
+                return "No User Found";
+            }
+        }
+
+       
+
+    }
+}
---
+++ Models/UserContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkCodeApp.Models
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions options) : base(options)
+        { 
+
+
+        }
+        public DbSet<Users> Users { get; set; }
+            
+        
+    }
+}
